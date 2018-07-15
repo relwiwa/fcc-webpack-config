d8b9280b3f5c4a07f9ad03dc7daf987fb3dec395
Walter <relw1wa-github@yahoo.de>
2018-07-15 16:12:07 +0200

    SSR: Setup webpack configuration for transpiling express server files

---
+++ webpack.config.server.js
@@ -0,0 +1,40 @@
+/*  Webpack configuration for server-side rendering
+    adapted from Stephen Grider's Udemy course about
+    SSR a React and Redux application */
+
+const webpackMerge = require('webpack-merge');
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+
+const webpackConfigCommon = require('./webpack.config.common');
+
+const webpackConfigServer = webpackMerge([
+  {
+    target: 'node',
+    mode: 'production',
+    entry: './express-server/server.js',
+
+    output: {
+      filename: 'server.js',
+      path: path.resolve(__dirname, '../', 'express-server', 'build-server'),
+    },
+
+    /*  prevents webpack from packing imported libraries into
+        the bundle on the server */
+    externals: [webpackNodeExternals()],
+
+    /* ignore all files but javascript when processing files on the server */
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          loader: 'ignore-loader',
+        },
+      ],
+    },
+  },
+]);
+
+const webpackConfigMerged = webpackMerge(webpackConfigCommon, webpackConfigServer);
+webpackConfigMerged.plugins = [];
+module.exports = webpackConfigMerged;
